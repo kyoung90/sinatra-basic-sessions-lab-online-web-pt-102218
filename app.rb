b8164db222296b898582f23107c9c415eedf5cbd
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-12 22:37:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
     enable :sessions
-    secret "My amazing cat"
+    set :session_secret, "My amazing cat"
   end
 end
